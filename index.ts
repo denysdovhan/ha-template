1a3fc079c9280498862097589e98b90965896ebb
Denys Dovhan <denysdovhan@gmail.com>
2023-06-09 19:19:11 +0300

    feat: add ts types

--- index.ts
+++ index.ts
@@ -3,13 +3,14 @@ import { LitElement, nothing } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 
 type UnsubscribePromise = Promise<() => Promise<void>>;
+type UnsubscribeError = Error & { code: string };
 
 @customElement('ha-template')
 export class HATemplate extends LitElement {
   @property() public hass!: HomeAssistant;
 
-  @property() public template: string = '';
-  @property() public variables: Record<string, any> = {};
+  @property() public template = '';
+  @property() public variables: Record<string, unknown> = {};
   @property() public value: string | null = null;
 
   @property({ attribute: false })
@@ -53,9 +54,9 @@ export class HATemplate extends LitElement {
         const unsubscribe = await this.unsubscribePromise;
         this.unsubscribePromise = null;
         return unsubscribe();
-      } catch (err: any) {
+      } catch (err: unknown) {
         // We don't care when connection is closed.
-        if (err.code !== 'not_found') {
+        if ((err as UnsubscribeError).code !== 'not_found') {
           throw err;
         }
       }
@@ -67,8 +68,8 @@ export class HATemplate extends LitElement {
   }
 }
 
-export default function register(component = 'ha-template') {
-  if (!customElements.get(component)) {
-    customElements.define(component, HATemplate);
+export default function register(componentName = 'ha-template') {
+  if (!customElements.get(componentName)) {
+    customElements.define(componentName, HATemplate);
   }
 }
